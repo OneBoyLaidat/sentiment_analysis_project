1af465cc905b22c40f48c8a07dbc0f129e9c3315
Ayomide Ifedayo <ayomideifedayomm@gmail.com>
2026-04-08 20:39:45 +0100

    Fix layout parameter in set_page_config function

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_title="Phlex Sentiment Dashboard", layout=wide)
+st.set_page_config(page_title="Phlex Sentiment Dashboard", layout="wide")
 
 st.title("Phlex Payment Customer Sentiment Analysis")
 st.markdown("Analysis of transaction complaints from Oct - Dec 2025.")
